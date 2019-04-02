2c1c3336eefcfa8feb039b351960a8431bea6ff6
dccasey <daniel.c.casey@gmail.com>
2019-04-01 21:08:39 -0400

    add xgboost as an importance metric

--- 02_assess_variables.R
+++ 02_assess_variables.R
@@ -6,6 +6,8 @@ library('mgcv')
 outdir = '/media/dan/variable_selection/v1/'
 nfolds = 5
 nrounds = 5
+source('~/Documents/code/umal_varselect/assess_variables_functions.R')
+
 #laod files
 dat = readRDS(paste0(outdir, 'extracted_variables.rds'))
 var_names = readRDS(paste0(outdir, 'extracted_var_names.rds'))
@@ -29,15 +31,18 @@ miss_assess = melt(miss_assess, variable.factor = F)
 goodies = miss_assess[value<.25, variable]
 
 #create test datasets (SRS & hold out a city or two)
-train_vals = sample(1:nrow(dat),size = round(.8 * nrow(dat)))
+train_vals = sample(1:nrow(dat),size = nrow(dat))
 dat[,id:=.I]
 dat[, cases := `PfPR2-10`/100 * Ex]
 dat[, not_cases := Ex - cases]
-test = dat[!id %in% train_vals, ]
+
+dat[, YEAR := as.numeric(substr(YEAR, 1, 4))]
+
+#test = dat[!id %in% train_vals, ]
 train = dat[train_vals,]
 
 #create kfolds
-folds = make_folds(nrow(rhs), nfolds, nrounds)
+folds = make_folds(nrow(train), nfolds, nrounds)
 
 #create model grid
 model_grid = expand.grid(var = goodies, fold = 1:nfolds, round = 1:nrounds, stringsAsFactors = F)
@@ -47,7 +52,7 @@ setDT(model_grid)
 all_mods = sapply(seq(nrow(model_grid)), function(x) fit_model(pr = train,
                                                                iv = model_grid[x,var],
                                                                flagflag = which(folds[,model_grid[x,round]]==model_grid[x,fold]),
-                                                               return_rmse = T))
+                                                               ret_opt = 'rmse'))
 #Compute overall RMSE
 model_grid[, rmse := all_mods]
 
@@ -60,6 +65,14 @@ best_prodvar[prod == 'MCD43A4' & !grepl('Reflectance', var_name, fixed = T), var
 best_prodvar = best_prodvar[best_prodvar[, .I[which.min(rmse)], by = c('var', 'prod')]$V1]
 
 new_goodies = best_prodvar[, var_name]
+
+#for the "goodies" variables, compute the spline transformation, all insample
+trans_vars = lapply(new_goodies, function(x) fit_model(pr = train,
+                                                       iv = x,
+                                                       ret_opt = 'spline_vals'))
+
+#add to the training dataset
+train[, paste0('s_',new_goodies) := trans_vars]
 
 #compute interactions
 inters = expand.grid(a = new_goodies, b = new_goodies, stringsAsFactors = F)
@@ -74,13 +87,16 @@ inters = inters[!(a_prod==b_prod & a_var == b_var), ]
 #drop two srtm variables
 inters = inters[!(b_prod == 'srtm' & a_prod == 'srtm'), ]
 
+#pass the spline transformed variables
+inters[, c('a','b') := list(paste0('s_',a), paste0('s_', b))]
+
 #analyze these additional models
 imodgrid = expand.grid(id = seq(nrow(inters)), fold = 1:nfolds, round = 1:nrounds)
 setDT(imodgrid)
 intermods = sapply(seq(nrow(imodgrid)), function(x) fit_model(pr = train,
                                                                iv = unlist(inters[imodgrid[x,id], .(a,b)]),
                                                                flagflag = which(folds[,imodgrid[x,round]]==imodgrid[x,fold]),
-                                                               return_rmse = T))
+                                                               ret_opt = 'rmse'))
 
 
 
@@ -88,7 +104,7 @@ imodgrid[, rmse:= intermods]
 inters[, rmse := imodgrid[, list(rmse = mean(rmse)), by = 'id'][,rmse]]
 
 #combine results
-res = rbind(best_prodvar[,list(var1 = var_name, rmse)], inters[, .(var1 = a, var2 = b, rmse)], fill = T)
+res = rbind(best_prodvar[,list(var1 = paste0('s_', var_name), rmse)], inters[, .(var1 = a, var2 = b, rmse)], fill = T)
 
 #remove duplicates
 res[, id := .I]
@@ -102,9 +118,39 @@ res[v2 == 2, vvv2 := var2]
 
 res = unique(res[,.(vvv1, vvv2, rmse)])
 res[, res_id := .I]
+setorder(res, +rmse)
+head(res)
+
+#for each unique individual predictor, identify the model that it works best in
+res[, v1_best := .I[which.min(rmse)], by = 'vvv1']
+res[!is.na(vvv2), v2_best := .I[which.min(rmse)], by = 'vvv2']
+
+#find the best participation by variables
+particp = rbind(res[, list(vvv = vvv1, best = v1_best)], res[!is.na(v2_best), list(vvv = vvv2, best = v2_best)])
+particp = particp[, list(best = min(best)), by = 'vvv']
 
 #for each of the original prodvars
+keepers = res[unique(particp[, best]), ]
+keepers[is.na(vvv2), finale := vvv1]
+keepers[!is.na(vvv2), finale := paste0(vvv1, '_', vvv2)]
 
-#take top fraction
+#compute interactions
+i_effs = keepers[!is.na(vvv2), ]
+for(iii in seq(nrow(i_effs))){
+  v1 = i_effs[iii, vvv1]
+  v2 = i_effs[iii, vvv2]
+  train[, paste0(v1,'_',v2) := get(v1) * get(v2)]
+}
+
+#check correlation (they all seem ok)
+correlate = cor(na.omit(train[,keepers[,finale],with = F]))
+rrr = row.names(correlate)
+correlate = data.table(correlate)
+correlate[, row_var := rrr]
+correlate = melt(correlate, id.vars = 'row_var', variable.factor = F)
+correlate = correlate[row_var != variable,]
+
+#compute importance via xgboost
+import = expand.grid(fold = 1:nfolds, round = 1:nrounds)
 
 #assess OOS fit
--- assess_variables_functions.R
+++ assess_variables_functions.R
@@ -7,10 +7,11 @@ make_folds = function(numrows, numfolds = 8, numsets = 1){
 }
 
 #For each variable, fit a GAM of the form cbind(cases, not cases) ~ s(variable) + reff(city) & capture AIC
-fit_model = function(pr, iv, flagflag = seq(nrow(pr)), return_rmse = F){
+fit_model = function(pr, iv, flagflag = seq(nrow(pr)), ret_opt = c('model', 'rmse', 'spline_vals')){
+  #print(iv)
   
   lhs = cbind(pr[,cases], pr[,not_cases])
-  rhs = pr[, c('city_name', iv, 'value'), with = F]
+  rhs = pr[, c('city_name', iv, 'value', 'YEAR'), with = F]
   
   if(length(iv)==1){
     setnames(rhs, iv, 'ivar')
@@ -23,8 +24,12 @@ fit_model = function(pr, iv, flagflag = seq(nrow(pr)), return_rmse = F){
   rhs[, wetdry := as.integer(value == 'Wet')]
   rhs[, city_name_id := as.numeric(as.factor(city_name))]
   setDF(rhs)
-  form = as.formula('lhs~1 + wetdry +  s(ivar) + s(city_name_id, bs = "re")')
   
+  if(length(unique(rhs[,'YEAR']))<=1){
+    form = as.formula('lhs~1 + wetdry + s(ivar) + s(city_name_id, bs = "re")')
+  }else{
+    form = as.formula('lhs~1 + wetdry + s(ivar) + s(city_name_id, bs = "re")')
+  }
   #subset by provided indices
   lhs_holdout= lhs[-flagflag,]
   lhs = lhs[flagflag,]
@@ -33,17 +38,89 @@ fit_model = function(pr, iv, flagflag = seq(nrow(pr)), return_rmse = F){
   
   mod = gam(form, family = 'binomial', data =rhs)
   
-  if(return_rmse){
+  if(ret_opt == 'rmse'){
     preds = c(predict(mod, rhs_holdout, type = 'response'))
     truuf = lhs_holdout[,1]/rowSums(lhs_holdout)
     rmse = RMSE(preds, truuf)
     return(rmse)
+  } else if(ret_opt == 'model'){
+    return(mod)
+  } else{
+    
+    svals = predict(mod, rhs, type = 'terms')
+    svals = svals[, which(colnames(svals) == 's(ivar)')]
+    
+    return(svals)
+    
   }
   
-  return(mod)
 }
 
 #no idea why I always have to look this up (https://stackoverflow.com/questions/26237688/rmse-root-mean-square-deviation-calculation-in-r)
 RMSE = function(m, o){
   sqrt(mean((m - o)^2, na.rm = T))
+}
+
+fit_xgb = function(pr, iv, flagflag = seq(nrow(pr)), ret_opt = c('model', 'rmse', 'preds', 'importance')){
+  
+  #prep dataset
+  pr = (pr[, c('city_name', iv, 'value', 'YEAR', 'cases', 'not_cases'), with = F])
+  pr[, N := cases + not_cases] 
+  pr[, wetdry := as.integer(value == 'Wet')]
+  pr[, city_name_id := as.factor(city_name)]
+  
+  setnames(pr, iv, paste0('ivar', 1:length(iv)))
+  ivars = paste0('ivar', 1:length(iv))
+  
+  cities = unique(pr[, city_name])
+  for(ccc in cities[2:length(cities)]){
+    pr[, (ccc) := as.numeric(ccc == city_name)]
+  }
+  
+  ptors = c(ivars, 'wetdry',cities[2:length(cities)])
+  
+  #subset by provided indices
+  rhs_holdout = pr[-flagflag, ptors, with = F]
+  rhs = pr[flagflag, c(ptors, 'N'), with = F]
+  lhs = pr[flagflag, cases/N]
+  lhs_holdout = pr[-flagflag, cases/N]
+  
+  #drop NAs from training
+  rhs[,id := .I]
+  r_id = rhs[,id]
+  rhs = na.omit(rhs)
+  drop_r = setdiff(r_id, rhs[,id])
+  lhs = lhs[-drop_r]
+  
+  #drop NAs from test
+  rhs_holdout[, id:= .I]
+  rh_id = rhs_holdout[, id]
+  rhs_holdout = na.omit(rhs_holdout)
+  drop_rh = setdiff(rh_id, rhs_holdout[,id])
+  lhs_holdout = lhs_holdout[-drop_rh]
+  
+  dm = xgboost::xgb.DMatrix(data = as.matrix(rhs[, ptors, with = F]),
+                            label = lhs,
+                            weight = rhs[, N])
+  
+  mod = xgboost::xgboost(data = as.matrix(rhs[, ptors, with = F]),
+                         label = lhs,
+                         weight = rhs[, N],
+                         params = list(objective = 'reg:logistic', nthread = 4, max_depth = 4, eta = .01,
+                                       subsample = .6, colsample_bytree = .75),
+                         nrounds = 3000, verbose = F)
+  
+  insamp = predict(mod, newdata = xgboost::xgb.DMatrix(data = as.matrix(rhs[,ptors, with = F])))
+  plot(lhs, insamp)
+  
+  if(ret_opt == 'model'){
+    return(xgboost::xgb.save.raw(mod))
+  }else if(ret_opt == 'rmse'){
+    preds = predict(mod, newdata = xgboost::xgb.DMatrix(data = as.matrix(rhs_holdout[,ptors, with = F])))
+    rmse = RMSE(preds, lhs_holdout)
+  }else if(ret_opt == 'preds'){
+    preds = predict(mod, newdata = xgboost::xgb.DMatrix(data = as.matrix(rhs[,ptors, with = F])))
+  }else{
+    return(xgboost:xgb.importance(model = mod))
+  }
 }
